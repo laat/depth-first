bae191003f26843bd511b64b223f93f3f4bd7f09
Sigurd Fosseng <sigurd@fosseng.net>
2017-05-14 02:53:00 +0200

    fix types, rewrite tests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // @flow
 export default function depthFirst<T>(
-  edges: Array<Array<T>>,
+  edges: Array<[T, T]>,
   node: T,
   opts?: { reverse: boolean },
 ): Array<T> {
   const { reverse } = Object.assign({}, { reverse: false }, opts);
-  edges = reverse ? edges.map(e => e.reverse()) : edges; // eslint-disable-line no-param-reassign
+  edges = reverse ? edges.map(([from, to]) => [to, from]) : edges; // eslint-disable-line no-param-reassign
   const result = [];
   const visited = new Map();
   const dfs = (v) => {
--- src/test.js
+++ src/test.js
@@ -4,17 +4,37 @@ import dfs from '.';
 
 it('finds nodes DFS', async () => {
   const edges = [
-    ['tie your shoes', 'put on your shoes'],
-    ['put on your jacket', 'put on your shirt'],
-    ['put on your shoes', 'put on your shorts'],
-    ['put on your jacket', 'put on your shorts'],
+    ['put on your shoes', 'tie your shoes'],
+    ['put on your shirt', 'put on your jacket'],
+    ['put on your shorts', 'put on your jacket'],
+    ['put on your shorts', 'put on your shoes'],
   ];
-  expect(dfs(edges, 'tie your shoes').sort()).toEqual([
-    'put on your shorts',
-    'put on your shoes',
-    'tie your shoes',
-  ].sort());
   expect(dfs(edges, 'put on your shorts').sort()).toEqual([
+    'put on your shorts',
+    'put on your jacket',
+    'put on your shoes',
+    'tie your shoes'
+  ].sort());
+  expect(dfs(edges, 'put on your shirt').sort()).toEqual([
+    'put on your shirt',
+    'put on your jacket',
+  ].sort());
+});
+
+it('finds nodes DFS reverse', async () => {
+  const edges = [
+    ['put on your shoes', 'tie your shoes'],
+    ['put on your shirt', 'put on your jacket'],
+    ['put on your shorts', 'put on your jacket'],
+    ['put on your shorts', 'put on your shoes'],
+  ];
+  expect(dfs(edges, 'put on your jacket', { reverse: true }).sort()).toEqual([
+    'put on your jacket',
+    'put on your shirt',
+    'put on your shorts',
+  ].sort());
+  expect(dfs(edges, 'put on your shoes', { reverse: true }).sort()).toEqual([
+    'put on your shoes',
     'put on your shorts',
   ].sort());
 });
@@ -25,21 +45,6 @@ it('DFS handles cycles', async () => {
     ['tie your shoes', 'put on your shoes'],
   ];
   expect(dfs(edges, 'put on your shoes').sort()).toEqual([
-    'put on your shoes',
-    'tie your shoes',
-  ].sort());
-});
-
-it('can be reversed', async () => {
-  const edges = [
-    ['tie your shoes', 'put on your shoes'],
-    ['put on your jacket', 'put on your shirt'],
-    ['put on your shoes', 'put on your shorts'],
-    ['put on your jacket', 'put on your shorts'],
-  ];
-  expect(dfs(edges, 'put on your shorts', { reverse: true }).sort()).toEqual([
-    'put on your jacket',
-    'put on your shorts',
     'put on your shoes',
     'tie your shoes',
   ].sort());
@@ -61,10 +66,5 @@ it('supports nodes without edges', async () => {
   ];
   expect(dfs(edges, 'listen to audiobook', { reverse: true }).sort()).toEqual([
     'listen to audiobook',
-  ].sort());
-
-  expect(dfs(edges, 'put on your shirt', { reverse: true }).sort()).toEqual([
-    'put on your shirt',
-    'put on your jacket',
   ].sort());
 });
