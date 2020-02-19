e830c2993af49e23d1f1720cbe9f269f3bc0d32c
Sigurd Fosseng <sigurd@fosseng.net>
2020-02-19 22:24:33 +0100

    cosmetic

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,24 @@ export type Edges<T> = Array<Edge<T>>;
 export default function depthFirst<T>(
   edges: Edges<T>,
   node: T,
-  opts?: { reverse: boolean }
+  opts: { reverse?: boolean } = {}
 ): Array<T> {
-  const { reverse } = Object.assign({}, { reverse: false }, opts);
+  const { reverse = false } = opts;
   edges = reverse ? edges.map(([from, to]) => [to, from]) : edges;
+  const outEdges = new Map<T, T[]>();
+  edges.forEach(([from, to]) => {
+    outEdges.get(from)?.push(to) ?? outEdges.set(from, [to]);
+  });
   const result: Array<T> = [];
-  const visited = new Map();
+  const visited = new Map<T, boolean>();
   const dfs = (v: T) => {
     result.push(v);
     visited.set(v, true);
-    edges
-      .filter(([from]) => from === v)
-      .forEach(([, to]) => {
-        if (!visited.has(to)) {
-          dfs(to);
-        }
-      });
+    for (const to of outEdges.get(v) ?? []) {
+      if (!visited.has(to)) {
+        dfs(to);
+      }
+    }
   };
   dfs(node);
   return result;
